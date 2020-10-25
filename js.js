f7087b9260188e98aca79b1198298002db57ecb5
jkunciene <j.kunciene@gmail.com>
2020-10-25 09:31:20 +0200

    Startinis projektas pamokelems, bureliams

---
+++ js.js
@@ -0,0 +1,8 @@
+//Setup website here
+let hero = document.querySelector("#hero");
+let villain = document.querySelector("#villain");
+let lightning = document.querySelector("#lightning");
+
+//Setup animation code here
+
+//Run animation code here
